04c0725233435cbf8f08c6c1c22441216c908eae
20181290 한찬희 <20181290@vision.hoseo.edu>
2022-10-23 17:47:29 +0900

    .

--- M_Test/Intermediate/Build/Win64/M_TestEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30146
-#include "D:/Documents/Unreal Projects/M_Test/Intermediate/Build/Win64/M_TestEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- M_Test/Source/M_Test/M_Test.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "M_Test.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, M_Test, "M_Test" );
- 
